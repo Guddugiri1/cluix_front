5a9ea2569f6680b9ce5895d4db8fe23817b875fb
Guddugiri1 <guddugiri216@gmail.com>
2024-10-05 00:33:54 +0530

    updated code

--- src/app/company/about-us/page.js
+++ src/app/company/about-us/page.js
@@ -1,6 +1,10 @@
 import AboutUsPageComponent from "@/components/AboutUsPage/AboutUsPageComponent";
 import React from "react";
-
+export const metadata = {
+  title: "Cluix",
+  description:
+    "Cluix is an advanced water purification system offering lightweight fluid resources, designed for complex applications and diverse topologies.",
+};
 const page = () => {
   return <AboutUsPageComponent />;
 };
