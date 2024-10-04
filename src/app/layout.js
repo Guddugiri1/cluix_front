a46b8e98ad985e90dbc3b64d2f3ebab3bc35b504
Guddugiri1 <guddugiri216@gmail.com>
2024-10-04 20:01:11 +0530

    meta data updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-// DEFINE ALL EXTERNAL IMPORT 
+// DEFINE ALL EXTERNAL IMPORT
 import { Inter } from "next/font/google";
 import { Roboto } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
@@ -14,7 +14,7 @@ import LayoutWrapper from "@/components/layout/LayoutWrapper/LayoutWrapper";
 // DEFINE INTER FONTS
 const inter = Inter({ subsets: ["latin"] });
 
-// DEFINE ROBOTO FONT 
+// DEFINE ROBOTO FONT
 const roboto = Roboto({
   weight: ["400", "700"],
   style: ["normal", "italic"],
@@ -26,13 +26,42 @@ const roboto = Roboto({
 export const metadata = {
   title: "Cluix",
   description:
-    "Cluix is a water purify system that provides fluid and lightweight fluid resources for applications that require more than one topology",
+    "Cluix is an advanced water purification system offering lightweight fluid resources, designed for complex applications and diverse topologies.",
+};
+
+export const productsMetadata = {
+  title: "Our Products",
+  description:
+    "Discover Cluix’s range of water purification systems, providing fluid and lightweight solutions tailored to your specific needs.",
+};
+
+export const productDetailsMetadata = {
+  title: "Product Details",
+  description:
+    "Get detailed insights into Cluix water purify systems, designed for efficiency and adaptable fluid solutions in multi-topology environments.",
+};
+
+export const aboutUsMetadata = {
+  title: "About Us",
+  description:
+    "Learn more about Cluix, a pioneer in water purification technology providing innovative and lightweight fluid resources for various applications.",
+};
+
+export const careersMetadata = {
+  title: "Careers",
+  description:
+    "Join the Cluix team and contribute to groundbreaking water purification systems with diverse opportunities for growth in fluid resource solutions.",
+};
+
+export const contactUsMetadata = {
+  title: "Contact Us",
+  description:
+    "Get in touch with Cluix for inquiries about our water purification systems and learn more about how our solutions can benefit your needs.",
 };
 
 // DEFINE MAIN FUNCTION WITH EXPORT
 export default function RootLayout({ children }) {
-
-  // DEFINE MAIN FUNCTION RETURN 
+  // DEFINE MAIN FUNCTION RETURN
   return (
     <html lang="en">
       <head>
